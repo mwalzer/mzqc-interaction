c52798d8c265e29192572dc4f17e48f2f3ba3157
mwalzer <walzer@ebi.ac.uk>
2023-08-22 16:09:42 +0000

    refactored init and default plot function returns

--- qc2_timeseries.py
+++ qc2_timeseries.py
@@ -28,8 +28,14 @@ dataset_mid_path = {"Lumos_2017": "PXD019888",
 metadata_paths = {k: os.path.join(os.path.join(mzqc_basepath, v), "metadata_"+k+".json") for k,v in dataset_mid_path.items()}
 mzqc_paths = {k: [os.path.join(os.path.join(mzqc_basepath, v), x) for x in os.listdir(os.path.join(mzqc_basepath, v)) if x.endswith(".mzqc")] for k,v in dataset_mid_path.items()}
 
+df_widget = None
+date_range_slider = None
+checkbox_group = None 
+ds_descriptor = None
+
 def extract_date(rowrunmzqc: qc.MzQcFile):
-  return next(iter([pd.to_datetime(cd.value) for cd in rowrunmzqc.runQualities[0].metadata.inputFiles[0].fileProperties if cd.accession=="MS:1000747"])).date()
+  rawdate = next(iter([pd.to_datetime(cd.value) for cd in rowrunmzqc.runQualities[0].metadata.inputFiles[0].fileProperties if cd.accession=="MS:1000747"]))
+  return pd.Timestamp(rawdate)
 
 def load_main_from_mzqc(rowrunname: str, rowrunmzqc: qc.MzQcFile):
   return {'Date': extract_date(rowrunmzqc),
@@ -82,30 +88,15 @@ def load_ds(ds_key, mzqc_paths=mzqc_paths):
   ds_main = pd.DataFrame(dfs_main)
   ds_pep = pd.concat(dfs_peps, axis=0)
   ds_tic = pd.concat(dfs_tics, axis=0)
-  ds_main = ds_main.astype({'Date':'datetime64[ns]'})
+  # ds_main = ds_main.astype({'Date':'datetime64[ns]'})
   ds_main.sort_values(by='Date', inplace = True) 
   ds_pep.sort_values(by='Date', inplace = True) 
   ds_tic.sort_values(by='Date', inplace = True) 
   
   return ds_main, ds_tic, ds_pep
 
-ds_key = next(iter(mzqc_paths.keys()))
-ds_main, ds_tic, ds_pep = load_ds(ds_key)
-ds_select = pn.widgets.Select(name='Select Dataset', options=list(mzqc_paths.keys()))
-ds_select.value = ds_key  # this is important
-
-df_widget_max_select = 6
-df_widget = pn.widgets.Tabulator(value=ds_main, selectable='checkbox',
-                                  show_index=False,
-                                  pagination='local', page_size=10,
-                                  disabled=True,
-                                  hidden_columns=["Name",'mzrange'])
-df_widget.selectable=df_widget_max_select
-setattr(df_widget, 'tics', ds_tic)  # squirrel all ds_ into the df_widget? 
-setattr(df_widget, 'peps', ds_pep)
-
 def plot_tics(selection=[], df_widget:pn.widgets.Tabulator=df_widget):
-  if len(selection)==0:
+  if (df_widget is None) or (len(selection)==0) or (type(df_widget.value) != pd.DataFrame) or (df_widget.value.shape[0]):
     return pd.DataFrame(columns=["RT", "Intensity"]).hvplot.line(title="Total Ion Chromatogram",
                                                             line_width=0.5, 
                                                             xlabel="Retentiontime [s]", 
@@ -113,21 +104,21 @@ def plot_tics(selection=[], df_widget:pn.widgets.Tabulator=df_widget):
                                                             frame_height=500,
                                                             frame_width=800,)
   
-  with pn.param.set_values(col[1][1], loading=True):
+  with pn.param.set_values(col[3][0], loading=True):
     ds_tic = df_widget.tics
-    selected_tic = ds_tic[ds_tic.Name.isin(ds_main.iloc[selection].Name)]
-    selected_tic["Date .. Name"] = selected_tic.Date.astype(str) + ".." + selected_tic.Name.str[-10:]
-    selected_tic = selected_tic.drop(columns=["# Peak","NativeID","Date","Name",]).sort_values(["RT"], axis = 0, ascending = True)
+    selected_tic = ds_tic[ds_tic.Name.isin(df_widget.value.iloc[selection].Name)]
+    selected_tic["str_date"] = selected_tic.Date.dt.strftime('%Y-%m-%d')
+    selected_tic["Date .. Name"] = selected_tic.str_date + ".." + selected_tic.Name.str[-10:]
+    selected_tic["Date .. Name"] = selected_tic["Date .. Name"].astype(str)
+    selected_tic = selected_tic[["RT","Intensity","Date .. Name"]].sort_values(["RT"], axis = 0, ascending = True)
     plot = selected_tic.hvplot.line(x="RT",y="Intensity", groupby=["Date .. Name"],
                                     title="Total Ion Chromatogram",
                                     xlabel="Retentiontime [s]", ylabel="Relative Intensity of Ion Current",
                                     frame_height=500, frame_width=800,).overlay()
     return plot 
 
-tics_pane = pn.bind(plot_tics, selection=df_widget.param.selection, df_widget=df_widget)
-
 def plot_runsticker(selection=[], df_widget:pn.widgets.Tabulator=df_widget):
-  if len(selection)==0:
+  if (df_widget is None) or (len(selection)==0) or (type(df_widget.value) != pd.DataFrame) or (df_widget.value.shape[0]):
     selection_df = pd.DataFrame(columns=['Date','# MS1','# MS2','# ID MS2','# Features','# ID Features', '# Signal fluct. ↓', '# Signal fluct. ↑',])
     idax = selection_df[['# MS1','# MS2', '# ID MS2', 'Date']].set_index('Date').hvplot.barh(frame_height=200, frame_width=200)
     qaax = selection_df[['# Features','# ID Features', 'Date']].set_index('Date').hvplot.barh(frame_height=200, frame_width=200)
@@ -135,9 +126,9 @@ def plot_runsticker(selection=[], df_widget:pn.widgets.Tabulator=df_widget):
                              invert=True, frame_height=200, frame_width=200).opts(yaxis='bare')
     flax = selection_df[['# Signal fluct. ↓', '# Signal fluct. ↑', 'Date']]\
             .set_index('Date').astype('int').hvplot.bar(rot=45, frame_height=200, frame_width=200)
-    return (idax + qaax + mzax + flax).cols(1).opts(shared_axes=False)
+    return (idax + qaax + mzax + flax).cols(2).opts(shared_axes=False)
 
-  with pn.param.set_values(col[1][2], loading=True):
+  with pn.param.set_values(col[3][1], loading=True):
     selection_df = df_widget.value.iloc[selection]
     idax = selection_df[['# MS1','# MS2', '# ID MS2', 'Date']].set_index('Date').hvplot.barh(frame_width=200, frame_height=200)
     qaax = selection_df[['# Features','# ID Features', 'Date']].set_index('Date').hvplot.barh(frame_width=200, frame_height=200)
@@ -154,51 +145,31 @@ def plot_runsticker(selection=[], df_widget:pn.widgets.Tabulator=df_widget):
                              frame_height=200).opts(yaxis='bare')
     flax = selection_df[['# Signal fluct. ↓', '# Signal fluct. ↑', 'Date']]\
             .set_index('Date').astype('int').hvplot.bar(rot=45, frame_width=200, frame_height=200)
-    return (idax + qaax + mzax + flax).cols(1).opts(shared_axes=False,)
-
-runsticker_pane = pn.bind(plot_runsticker, selection=df_widget.param.selection, df_widget=df_widget)
-
-date_range_slider = pn.widgets.DateRangeSlider(
-    name='Date Range Slider',
-    start=ds_main.Date.min(), end=ds_main.Date.max(),
-    # value=(next(iter(ds_dfs.values())).Date.min()+dt.timedelta(days=10), 
-    #       next(iter(ds_dfs.values())).Date.max()-dt.timedelta(days=10)),
-    value=(ds_main.Date.min(), ds_main.Date.max()),
-    tooltips=True,
-)
-
-checkbox_group = pn.widgets.CheckBoxGroup(
-    name='Checkbox Group', value=['# MS1', '# MS2'], 
-    options=ds_main.columns.drop(['Date', 'RT range', 'MZ range', 'mzrange', 'Name']).to_list(),
-    inline=False
-)
+    return (idax + qaax + mzax + flax).cols(2).opts(shared_axes=False,)
 
 def plot_metrics_daterange(start, end, selection, df_widget:pn.widgets.Tabulator=df_widget):
-  if (not selection):
-    pass
+  if (df_widget is None) or (len(selection)==0) or (type(df_widget.value) != pd.DataFrame) or (df_widget.value.shape[0]):
+    return pd.DataFrame(columns=["Date","Value"]).hvplot.line(xlabel='Date', title='Single Metrics Timeseriesplot')
 
   df = df_widget.value
   truncated = df.loc[:, (df.columns[(df.columns.str.startswith(('#','Date'))) & (df.columns.isin(selection+['Date']))])]
   truncated.sort_values(by='Date', inplace = True)
   print("df", type(truncated.Date[0]))  
-  print("df*", type(truncated.Date.dt.date[0]))
   print("slider", type(start))
-  line_plot = truncated[(truncated.Date >=  start) & (truncated.Date <=  end)].hvplot.line(x='Date', title='Line Plot')
-  line_plot = df.hvplot.line(x='Date', title='Single Value Metrics')
+  truncated = truncated[(truncated.Date.dt.date >= start.date()) & (truncated.Date.dt.date <= end.date())]
+  print(truncated)
+  line_plot = truncated.hvplot.line(x='Date', title='Single Metrics Timeseriesplot')
   return line_plot
 
-metrics_pane = pn.bind(plot_metrics_daterange, 
-                       start=date_range_slider.param.value_start, end=date_range_slider.param.value_end,
-                       selection=checkbox_group.param.value,)
-
 def plot_ccharts(start, end, df_widget:pn.widgets.Tabulator=df_widget):
-  if any(['peps','peps_mean','peps_std']):
+  if (df_widget is None) or (type(df_widget.value) != pd.DataFrame) or (df_widget.value.shape[0]==0) or\
+    (any([getattr(df_widget,x,None) is None for x in ['peps','peps_mean','peps_std']])):
     layout = hv.Layout([pd.DataFrame(columns=["Date","mean per day"]).hvplot.line()]*4).cols(2).opts(shared_axes=False)
     return layout
   
   pep_df = df_widget.peps
 
-  filtered_pep_df = pep_df[(pep_df['Date'] >=  pd.Timestamp(start)) & (pep_df['Date'] <=  pd.Timestamp(end))]
+  filtered_pep_df = pep_df[(pep_df['Date'].dt.date >=  start.date()) & (pep_df['Date'].dt.date <=  end.date())]
   filtered_pep_df_means = pd.DataFrame({
       "dRT": filtered_pep_df.groupby(['Date','Name'])['dRT'].mean(),
       "dPPM": filtered_pep_df.groupby(['Date','Name'])['dPPM'].mean(),
@@ -232,10 +203,6 @@ def plot_ccharts(start, end, df_widget:pn.widgets.Tabulator=df_widget):
   layout = hv.Layout(figs).cols(2).opts(shared_axes=False)
   return layout
 
-cchart_pane = pn.bind(plot_ccharts, 
-                       start=date_range_slider.param.value_start, end=date_range_slider.param.value_end,
-                       df_widget=df_widget)
-
 def update_ds_pep_stats(df_widget:pn.widgets.Tabulator=df_widget):
   pep_df = df_widget.peps
   tdf = pd.merge(pep_df, pd.DataFrame({"mean_rt_pp": pep_df.groupby(['Peptide'])['RT'].mean()}), how="inner", on="Peptide")
@@ -258,6 +225,109 @@ def update_ds_pep_stats(df_widget:pn.widgets.Tabulator=df_widget):
   }
   setattr(df_widget, 'peps_mean', pep_df_mean)
 
+def plot_calendar_hist(df_widget_value, df_widget:pn.widgets.Tabulator=df_widget):
+  if (df_widget is None) or (type(df_widget.value != pd.DataFrame)) or (df_widget_df.shape[0]):
+    return pd.DataFrame(columns=["Date","Value"]).hvplot.line(
+          title="QC2 Run Yearly Distribution", 
+          ylabel="# Runs",
+          xlabel="Month of {}".format("N/A"))
+
+  df_widget_df = df_widget.value
+  calendar_hist = df_widget_df.assign(month=df_widget_df.Date.dt.month)\
+        .groupby(by=['month']).count().hvplot.bar(y="Date")\
+        .opts(title="QC2 Run Yearly Distribution", ylabel="# Runs",
+              xlabel="Month of {}".format(next(iter(df_widget_df.Date.dt.year))))
+  return calendar_hist
+
+def update_ds(ds_key, mzqc_paths=mzqc_paths, df_widget:pn.widgets.Tabulator=df_widget, ds_descriptor:pn.pane.Markdown=ds_descriptor,
+              date_range_slider:pn.widgets.DateRangeSlider=date_range_slider, checkbox_group:pn.widgets.CheckBoxGroup=checkbox_group):
+  # sanity check
+  if ds_key not in mzqc_paths.keys():
+    print("ds_key not in mzqc_paths.")
+    return
+
+  if df_widget is None:
+    print(">>> skipping enpty init", ds_key, df_widget)
+    return
+
+  print(">>> updating with {}".format(ds_key))
+
+  # load new dataset
+  main, tic, pep = load_ds(ds_key, mzqc_paths)
+  # update df_widget
+
+  df_widget.selection = list()
+  setattr(df_widget, 'tics', tic)
+  setattr(df_widget, 'peps', pep)
+  df_widget.value = main
+  update_ds_pep_stats(df_widget) 
+
+  print(">>> updated df_widget")
+
+  # update checkbox_group
+  dir(checkbox_group)
+  setattr(checkbox_group, 'options', main.columns.drop(['Date', 'RT range', 'MZ range', 'mzrange', 'Name']).to_list())  # do not deactivate unless all mzQC produce the same column layout
+  setattr(checkbox_group, 'value', ['# MS1', '# MS2'])
+
+  print(">>> updated chekcbox_group")
+
+  # update date_range_slider
+  date_range_slider.start=dt.datetime.combine(main.Date.min(),dt.datetime.min.time())
+  date_range_slider.end=dt.datetime.combine(main.Date.max(),dt.datetime.min.time())
+  date_range_slider.value=(date_range_slider.start,date_range_slider.end)
+
+  print(">>> updated date_range_slider")
+
+  ds_descriptor.value = descr_templ.format(s=str(main.Date.min().date()),
+                                          e=str(main.Date.max().date()),
+                                          p=dataset_mid_path[ds_key],
+                                          n=str(len(main)),
+                                          i="Thermo {}".format(next(iter(ds_key.split('_')))))
+
+  print(">>> updated ds_descriptor")
+
+  return
+
+
+df_widget_max_select = 6
+df_widget = pn.widgets.Tabulator(value=pd.DataFrame(columns=
+  ['Date','# MS1','# MS2','# ID MS2','# Features','# ID Features', '# Signal fluct. ↓', '# Signal fluct. ↑','Name','mzrange',]),
+                                  selectable='checkbox',
+                                  show_index=False,
+                                  pagination='local', page_size=10,
+                                  disabled=True,
+                                  hidden_columns=["Name",'mzrange'])
+df_widget.selectable=df_widget_max_select
+setattr(df_widget, 'tics', None)  # squirrel all ds_ into the df_widget? 
+setattr(df_widget, 'peps', None)
+
+ds_select = pn.widgets.Select(name='Select Dataset', options=list(mzqc_paths.keys()))
+
+tics_pane = pn.bind(plot_tics, selection=df_widget.param.selection)
+
+runsticker_pane = pn.bind(plot_runsticker, selection=df_widget.param.selection)
+
+date_range_slider = pn.widgets.DateRangeSlider(
+    name='Date Range Slider',
+    start=dt.datetime.now()-dt.timedelta(days=1), end=dt.datetime.now()+dt.timedelta(days=1),
+    value=( dt.datetime.now()-dt.timedelta(days=1), dt.datetime.now()+dt.timedelta(days=1) ),
+    tooltips=True)
+
+checkbox_group = pn.widgets.CheckBoxGroup(
+    name='Checkbox Group', value=[], 
+    options=[],
+    inline=False
+)
+
+metrics_pane = pn.bind(plot_metrics_daterange, 
+                       start=date_range_slider.param.value_start, end=date_range_slider.param.value_end,
+                       selection=checkbox_group.param.value,)
+
+cchart_pane = pn.bind(plot_ccharts, 
+                       start=date_range_slider.param.value_start, 
+                       end=date_range_slider.param.value_end,
+                       )
+
 descr_templ = """
 # The Data Set
 
@@ -275,48 +345,9 @@ ds_descriptor = pn.pane.Markdown(descr_templ.format(s=str(dt.datetime.now().date
                                                                n=str(len([])),
                                                                i="<Make> <Model>"))
 
-def plot_calendar_hist(df_widget_df:pd.DataFrame=df_widget.value):
-  calendar_hist = df_widget_df.assign(month=df_widget_df.Date.dt.month)\
-        .groupby(by=['month']).count().hvplot.bar(y="Date")\
-        .opts(title="QC2 Run Yearly Distribution", ylabel="# Runs",
-              xlabel="Month of {}".format(next(iter(ds_main.Date.dt.year))))
-  return calendar_hist
+calendar_hist_pane = pn.bind(plot_calendar_hist, df_widget_value=df_widget.param.value,)
 
-calendar_hist_pane = pn.bind(plot_calendar_hist, df_widget_df=df_widget.param.value,)
-
-def update_ds(ds_key, mzqc_paths=mzqc_paths, df_widget:pn.widgets.Tabulator=df_widget, ds_descriptor=ds_descriptor,
-              date_range_slider:pn.widgets.DateRangeSlider=date_range_slider, checkbox_group:pn.widgets.CheckBoxGroup=checkbox_group):
-  # sanity check
-  if ds_key not in mzqc_paths.keys() or\
-     type(checkbox_group)!= pn.widgets.CheckBoxGroup:
-    print("Init phase has incomplete objects.")
-    return
-  # load new dataset
-  main, tic, pep = load_ds(ds_key, mzqc_paths)
-  # update df_widget
-  df_widget.value = main
-  df_widget.selection = list()
-  setattr(df_widget, 'tics', tic)
-  setattr(df_widget, 'peps', pep)
-  update_ds_pep_stats(df_widget) 
-
-  # update checkbox_group
-  dir(checkbox_group)
-  setattr(checkbox_group, 'options', main.columns.drop(['Date', 'RT range', 'MZ range', 'mzrange', 'Name']).to_list())  # do not deactivate unless all mzQC produce the same column layout
-  setattr(checkbox_group, 'value', ['# MS1', '# MS2'])
-  # update date_range_slider
-  date_range_slider.start=main.Date.min()
-  date_range_slider.end=main.Date.max()
-  date_range_slider.value=(main.Date.min(), main.Date.max())
-
-  ds_descriptor.value = descr_templ.format(s=str(main.Date.min().date()),
-                                                               e=str(main.Date.max().date()),
-                                                               p=dataset_mid_path[ds_key],
-                                                               n=str(len(main)),
-                                                               i="Thermo {}".format(next(iter(ds_key.split('_')))))
-  return
-
-ds_switch = pn.bind(update_ds, ds_key=ds_select.param.value, df_widget=df_widget, date_range_slider=date_range_slider, checkbox_group=checkbox_group, )
+ds_switch = pn.bind(update_ds, ds_key=ds_select.param.value)
 
 row0 = pn.Row(ds_select)
 row1 = pn.Row(ds_descriptor, calendar_hist_pane)
@@ -324,8 +355,8 @@ row2 = pn.Row(df_widget)
 row3 = pn.Row(tics_pane, runsticker_pane)
 internal_col = pn.Column(date_range_slider, checkbox_group)
 row4 = pn.Row(internal_col, metrics_pane)
-row5 = pn.Row(tabs = pn.Tabs(('Xbar control chart',cchart_pane), tabs_location='left'))   #,('Rbar control chart', cchart_pane)
-col = pn.Column(row0, row1, row2, row3, row4, row5, ds_switch)  # bind must be included to be active
+# row5 = pn.Row(tabs = pn.Tabs(cchart_pane, tabs_location='left'))   #,('Rbar control chart', cchart_pane)
+col = pn.Column(row0, row1, row2, row3, row4, cchart_pane, ds_switch)  # bind must be included to be active
 col.servable()
 
 
